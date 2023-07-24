447485a2a4dbaa3c2bcbe484fc5c0ea896f295bb
webexpert4pooja <amankamboj2387@gmail.com>
2023-07-24 15:08:46 +0530

    Fixing text form utils

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,6 +20,12 @@ function TextForm(props) {
         let currentText= event.target.value;
         setText(currentText);
     }
+    const handelCopy= ()=>{
+        let copy_txt= document.getElementById("myBox");
+        copy_txt.select();
+        navigator.clipboard.writeText(copy_txt.value);
+        props.showAlert("Text Copied Successfully","info");
+    }
     // usestate to handel the side effect 
     const [text,setText]=useState("");
   return (
@@ -27,10 +33,11 @@ function TextForm(props) {
     <div className={`container text-${props.mode==='light'?'dark':'light'}`}>
     <h1>{props.heading}</h1>
   <div className="mb-3">  
-  <textarea onChange={handelUpChange} className={`form-control mb-3 text-${props.mode==='light'?'dark':'light'} bg-${props.mode}`} rows="8" value={text}></textarea>
-  <button className="btn btn-primary mx-1" onClick={handelUpClick}>Convert Uppercase</button>
-  <button className="btn btn-danger mx-1" onClick={handelResetClick}>Reset</button>
-  <button className="btn btn-info" onClick={handelLowerClick}>Convert Lower</button>
+  <textarea id='myBox' onChange={handelUpChange} className={`form-control mb-3 text-${props.mode==='light'?'dark':'light'} bg-${props.mode}`} rows="8" value={text}></textarea>
+  <button disabled={text.length===0} className="btn btn-primary mx-1" onClick={handelUpClick}>Convert Uppercase</button>
+  <button disabled={text.length===0} className="btn btn-danger mx-1" onClick={handelResetClick}>Reset</button>
+  <button disabled={text.length===0} className="btn btn-info" onClick={handelLowerClick}>Convert Lower</button>
+  <button disabled={text.length===0} className='btn btn-warning mx-1' onClick={handelCopy}>Copy Text</button>
     </div>
     <h2>Your Text Summary</h2>
     <p>{text.split(" ").filter((element)=>{ return element.length!==0 }).length} words and {text.length} characters</p>
